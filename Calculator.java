4adff13bb69f3d187a877bdc6452ebb07498ea4f
saurabh s <ssehrawat@xyz.com>
2019-10-14 12:39:20 +0530

    changes from clone folder

--- Calculator.java
+++ Calculator.java
@@ -14,6 +14,6 @@ public class Calculator
 	System.out.println("Subtraction is "+(x-y));
 	System.out.println("Multiplication is "+(x*y));
 	System.out.println("Division is "+(x/y));
-	System.out.println("Changes made for GIT")
+	System.out.println("Changes made for GIT");
 	}
 }
